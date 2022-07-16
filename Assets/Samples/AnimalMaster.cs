1fdd382dc5899285ff1314ba8d4488c203544da0
ChapaCactus <moboratory@gmail.com>
2022-07-16 15:46:12 +0900

    AnimalMaster

--- Assets/Samples/AnimalMaster.cs
+++ Assets/Samples/AnimalMaster.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace MOB.ScriptableMasterSystem.Samples
 {
     /// <summary>
@@ -5,5 +7,12 @@ namespace MOB.ScriptableMasterSystem.Samples
     /// </summary>
     public sealed class AnimalMaster : MasterBase
     {
+        [SerializeField] private string _animalName;
+        [SerializeField] private string _bark;
+
+        /// <summary>動物の名称</summary>
+        public string AnimalName => _animalName;
+        /// <summary>鳴き声</summary>
+        public string Bark => _bark;
     }
 }
